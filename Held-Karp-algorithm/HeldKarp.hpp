a04523c3a72ae26a107a8ee7d72d2a129230b940
mikymaione <mikymaione@hotmail.it>
2020-03-19 18:56:52 +0100

    using lambda

--- Held-Karp-algorithm/HeldKarp.hpp
+++ Held-Karp-algorithm/HeldKarp.hpp
@@ -1,6 +1,6 @@
 ﻿/*
 MIT License
-Copyright (c) 2019: Michele Maione
+Copyright (c) 2020: Michele Maione
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
@@ -19,11 +19,12 @@ unsigned int
 #pragma once
 
 #include <algorithm>
-#include <array>
 #include <chrono>
+#include <functional>
 #include <iostream>
 #include <map>
 #include <set>
+#include <stack>
 #include <string>
 #include <vector>
 
@@ -32,8 +33,6 @@ using namespace std;
 class HeldKarp
 {
 private:
-	map<unsigned char, unsigned long long> FACT;
-
 	string PrintTour(map<unsigned long, map<unsigned char, unsigned char>> P, const unsigned char N)
 	{
 		string path;
@@ -57,67 +56,31 @@ private:
 		return path;
 	}
 
-	unsigned long long factorial(unsigned char n)
+	void Combinations(const int K, const int N, function<void(vector<unsigned char>)> CALLBACK)
 	{
-		if (FACT.count(n) == 0)
+		vector<unsigned char> result(K);
+
+		stack<unsigned char> stack;
+		stack.push(0);
+
+		while (stack.size() > 0)
 		{
-			unsigned long long r = 1;
+			auto index = stack.size() - 1;
+			auto value = stack.top();
+			stack.pop();
 
-			for (unsigned char i = 1; i <= n; i++)
-				r *= i;
+			while (value < N)
+			{
+				result[index++] = ++value;
+				stack.push(value);
 
-			FACT[n] = r;
+				if (index == K)
+				{
+					CALLBACK(result);
+					break;
+				}
+			}
 		}
-
-		return FACT[n];
-	}
-
-	unsigned long long combinations(unsigned char N, unsigned char K)
-	{
-		return factorial(N) / (factorial(K) * factorial(N - K));
-	}
-
-	unsigned char ** GetAllCombos_Permutation(const unsigned long long tot_combinations, const unsigned char N, const unsigned char K)
-	{
-		string bitmask(K, 1);
-		bitmask.resize(N, 0);
-
-		unsigned long z = 0;
-		short e = -1;
-
-		auto sets = new unsigned char*[tot_combinations];
-
-		do
-		{
-			e = -1;
-			sets[z] = new unsigned char[K];
-
-			for (unsigned char i = 0; i < N; ++i)
-				if (bitmask[i])
-					sets[z][e += 1] = (i + 1);
-
-			z++;
-		} while (prev_permutation(bitmask.begin(), bitmask.end()));
-
-		return sets;
-	}
-
-	template <class T>
-	unsigned long Powered2Code(const unsigned char N, T * S)
-	{
-		return Powered2Code(N, S, UCHAR_MAX);
-	}
-
-	template <class T>
-	unsigned long Powered2Code(const unsigned char N, T * S, const unsigned char exclude)
-	{
-		unsigned long code = 0;
-
-		for (unsigned long i = 0; i < N; i++)
-			if (S[i] != exclude)
-				code += 1 << S[i];
-
-		return code;
 	}
 
 	template <class IEnumerable>
@@ -138,16 +101,6 @@ private:
 		return code;
 	}
 
-	template <class T>
-	bool arrayContains(const unsigned long N, T * arr, T e)
-	{
-		for (unsigned long i = 0; i < N; i++)
-			if (arr[i] == e)
-				return true;
-
-		return false;
-	}
-
 public:
 	/*
 	Implementation of [Held-Karp](https://en.wikipedia.org/wiki/Held-Karp_algorithm), an algorithm that solves the Traveling Salesman Problem using dynamic programming with memoization.
@@ -160,10 +113,10 @@ public:
 	{
 		auto begin = chrono::steady_clock::now();
 
-		const unsigned char N0 = N - 1;
-
-		unsigned char π;
+		unsigned char π;;
 		unsigned short opt;
+
+		const unsigned char N0 = N - 1;
 
 		map<unsigned long, map<unsigned char, unsigned short>> C;
 		map<unsigned long, map<unsigned char, unsigned char>> P;
@@ -178,23 +131,17 @@ public:
 
 		for (unsigned char K = 1; K < N; K++) // cardinalità O(N)
 		{
-			auto tot_combinations = combinations(N0, K);
-			auto permutation_of_k = GetAllCombos_Permutation(tot_combinations, N0, K);
-
-			for (unsigned long long pk = 0; pk < tot_combinations; pk++) // sets O(2ⁿ)
+			Combinations(K, N0, [&](vector<unsigned char> &S)
 			{
-				auto S = permutation_of_k[pk];
-
 				for (unsigned char k = 1; k < N; k++) // nodi O(N)
-					if (!arrayContains(K, S, k))
+					if (!binary_search(S.begin(), S.end(), k))
 					{
 						π = 0;
 						opt = USHRT_MAX;
 
-						for (unsigned char b = 0; b < K; b++)
+						for each (auto m in S)
 						{
-							auto m = S[b];
-							auto tmp = C[Powered2Code(K, S, m)][m] + distance[k][m];
+							auto tmp = C[Powered2Code(S, m)][m] + distance[k][m];
 
 							if (tmp < opt)
 							{
@@ -203,19 +150,14 @@ public:
 							}
 						}
 
-						C[Powered2Code(K, S)][k] = opt;
-						P[Powered2Code(K, S)][k] = π;
+						C[Powered2Code(S)][k] = opt;
+						P[Powered2Code(S)][k] = π;
 					}
-			}
-
-			for (unsigned long long i = 0; i < tot_combinations; i++)
-				delete[] permutation_of_k[i];
-
-			delete[] permutation_of_k;
+			});
 		}
 
-		opt = USHRT_MAX;
 		π = 0;
+		opt = USHRT_MAX;
 
 		for each(auto k in FullSet) // O(N)
 		{
