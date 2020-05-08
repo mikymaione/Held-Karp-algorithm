1fee7cce9793bec2f0961f5204b7ac3b246ef93c
mikymaione <mikymaione@hotmail.it>
2020-05-08 15:03:25 +0200

    aggiunto BranchAndBound

--- Held-Karp-algorithm/Held-Karp-algorithm/main.cpp
+++ Held-Karp-algorithm/Held-Karp-algorithm/main.cpp
@@ -5,17 +5,17 @@ Permission is hereby granted, free of charge, to any person obtaining a copy of 
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 #include <iostream>
-#include "HeldKarp_GR.hpp"
+#include "BranchAndBound.hpp"
 
 void TSP(const vector<vector<uint_least8_t>> &DistanceMatrix2D)
 {
-	HeldKarp_GR hk(DistanceMatrix2D);
+	BranchAndBound hk(DistanceMatrix2D);
 	hk.TSP();
 }
 
 void TSP_RND(const uint_least8_t NumberOfNodes)
 {
-	const auto DistanceMatrix2D = HeldKarp_GR::New_RND_Distances(NumberOfNodes);
+	const auto DistanceMatrix2D = BranchAndBound::New_RND_Distances(NumberOfNodes);
 	TSP(DistanceMatrix2D);
 }
 
